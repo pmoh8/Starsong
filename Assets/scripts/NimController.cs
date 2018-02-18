baf2ab2a43d699b1f72117aea3dc4c1603de4b59
Phoebe Moh <mohphoebe@gmail.com>
2018-02-18 10:19:51 -0600

    Initial commit of demo stuff

---
+++ Assets/scripts/NimController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NimController : MonoBehaviour {
+    public float speed = 10;
+    public float groundSpeed = 80;
+    public float airSpeed = 5;
+    public float jump = 1000;
+    public float maxSpeed = 7;
+
+    private Rigidbody rb;
+
+    public int direction = 1;
+    public Quaternion lookLeft = Quaternion.Euler(0, 0, 0);
+    public Quaternion lookRight = Quaternion.Euler(0, 180, 0);
+
+    private Animator animator;
+
+    private bool multiJumpAllowed = true;
+    public int maxJumps = 3;
+    public int jumpCount;
+    public float nextJumpTime;
+    public float jumpBuffer=.2f;
+    GameObject star;
+    //private Transform myTransform;
+    // Use this for initialization
+    void Start () {
+        star = GameObject.Find("Star");
+
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        direction = 1;
+        Physics.gravity = new Vector3(0, -40F, 0);
+
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isOnGround())
+        {
+            jumpCount = maxJumps;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround())
+        {
+            Vector3 movement = new Vector3(0, jump, 0);
+            rb.AddForce(movement);          
+        }
+        float mvmtHoriz = Input.GetAxis("Horizontal");
+
+        if (mvmtHoriz != 0)
+        {
+            animator.SetInteger("State", 1);
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+        }
+    }
+
+    //for the physics
+    private void FixedUpdate(){
+        setSpeed();
+
+        //determing direction to face
+        float mvmtHoriz = Input.GetAxis("Horizontal");
+        float mvmtVert = Input.GetAxis("Vertical");
+        walk(mvmtHoriz);
+        if (Input.GetKeyDown(KeyCode.Z) && !isOnGround() && jumpCount > 0 && Time.time > nextJumpTime && multiJumpAllowed)
+        {
+            multiJump(mvmtHoriz, mvmtVert);
+        }
+
+    }
+    private void walk(float mvmtHoriz)
+    {
+        if (rb.velocity.magnitude > maxSpeed && isOnGround())
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        if (mvmtHoriz < 0 && direction == 1)
+        {
+            direction = -1;
+            transform.rotation = lookRight;
+
+        }
+        else if (mvmtHoriz > 0 && direction == -1)
+        {
+            direction = 1;
+            transform.rotation = lookLeft;
+        }
+        Vector3 movement = new Vector3(mvmtHoriz, 0, 0);
+        rb.AddForce(movement * speed);
+    }
+    private void multiJump(float mvmtHoriz, float mvmtVert)
+    {
+        cancelMomentum();
+        star.GetComponent<starController>().hardResetPosition();
+        Vector3 mvmt = new Vector3(mvmtHoriz*.75f, mvmtVert+40f/1000f, 0) * 800;
+        rb.AddForce(mvmt);
+        star.GetComponent<Rigidbody>().AddForce(-.7f * mvmt);
+        jumpCount--;
+        nextJumpTime = Time.time + jumpBuffer;
+        Debug.Log(mvmt);
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathPlane"|| col.gameObject.name == "Enemy")
+        {
+            death();
+        }
+    }
+    private void death()
+    {
+        cancelMomentum();
+        transform.position = new Vector3(0, 0, 0);
+        star.transform.position = new Vector3(1, 1, 1);
+    }
+    private void cancelMomentum()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+        rb.angularVelocity = new Vector3(0, 0, 0);
+    }
+    bool isOnGround() {
+        return Physics.Raycast(transform.position, -Vector3.up, 1);
+    }
+    void setSpeed()
+    {
+        if (isOnGround())
+        {
+            speed = groundSpeed;
+        }
+        else
+        {
+            speed = airSpeed;
+        }
+    }
+
+}
---
+++ Assets/starController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starController : MonoBehaviour {
+    private Rigidbody rb;
+    GameObject player;
+    float offset = 1f;
+    public Vector3 speed = new Vector3(3, 0, 0);
+    private bool attack = false;
+    private float returnTime;
+    private float nextFire;
+    NimController playerScript;
+    // Use this for initialization
+    void Start () {
+		rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Nim");
+        playerScript = player.GetComponent<NimController>();
+        Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        
+        Vector3 destination = player.transform.position;
+        if (Input.GetKeyDown(KeyCode.X) && Time.time > nextFire)
+        {
+            attack = true;
+            returnTime = Time.time + .05f;
+            nextFire = Time.time + .5f;
+        }
+
+        if (attack)
+        {
+            destination.x = destination.x + 20 * playerScript.direction;
+            destination.y = destination.y - .5f;
+            transform.position = Vector3.Lerp(rb.position, destination, 5*Time.deltaTime);
+            if (Time.time>returnTime)
+            {
+                attack = false;
+            }
+        }
+        else
+        {
+            resetPosition();
+        }
+
+    }
+    void resetPosition()
+    {
+        Vector3 destination = player.transform.position;
+        destination.x = destination.x - offset * playerScript.direction;
+        destination.y = destination.y + .2f;
+        destination.z = destination.z - .2f;
+        transform.position = Vector3.Lerp(rb.position, destination, 3 * Time.deltaTime);
+    }
+    public void hardResetPosition()
+    {
+        Vector3 destination = player.transform.position;
+        destination.x = destination.x - offset * playerScript.direction;
+        destination.y = destination.y + .2f;
+        destination.z = destination.z - .2f;
+        transform.position = destination;
+    }
+}
